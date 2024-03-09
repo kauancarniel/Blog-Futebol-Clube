e89630eedd58d3aa91c7df1cabe6a78be244b1ff
Kauan Carniel Peres <kauan-carniel@hotmail.com>
2024-03-09 20:55:42 -0300

    validações para o endpoint post da rota matches

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import MatchService from '../services/MatchService';
+import TeamService from '../services/TeamService';
 
 export default class MatchController {
   constructor(
     private matchService = new MatchService(),
+    private teamService = new TeamService(),
   ) {}
 
   public async findAll(req: Request, res: Response) {
@@ -31,6 +33,17 @@ export default class MatchController {
 
   public async newMatch(req: Request, res: Response) {
     const { homeTeamId, awayTeamId, homeTeamGoals, awayTeamGoals } = req.body;
+
+    if (homeTeamId === awayTeamId) {
+      return res.status(422)
+        .json({ message: 'It is not possible to create a match with two equal teams' });
+    }
+    const teamHome = await this.teamService.findById(Number(homeTeamId));
+    const teamAway = await this.teamService.findById(Number(awayTeamId));
+
+    if (!teamHome || !teamAway) {
+      return res.status(404).json({ message: 'There is no team with such id!' });
+    }
 
     const match = await this.matchService.newMatch(
       { homeTeamId, homeTeamGoals, awayTeamId, awayTeamGoals, inProgress: true },
