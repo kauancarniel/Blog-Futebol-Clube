f84d509ed77907f7a629f7f279090e599edf11eb
Kauan Carniel <kauan-carniel@hotmail.com>
2025-03-21 11:15:09 -0300

    config frontend deploy

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+  baseURL: `https://blog-futebol-clube-production.up.railway.app/ || '3001'}`,
 });
 
 export const setToken = (token) => {
