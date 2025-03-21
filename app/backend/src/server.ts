99d8fff31c914c2bd1f53781127186a66aaddcd0
Kauan Carniel <kauan-carniel@hotmail.com>
2025-03-21 14:20:15 -0300

    fix

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,4 +1,5 @@
 import { App } from './app';
 
 const PORT = process.env.PORT || 3001;
+console.log("test");
 new App().start(PORT);
--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `https://blog-futebol-clube-production.up.railway.app/`,
+  baseURL: `https://blog-futebol-clube-production.up.railway.app`,
 });
 
 export const setToken = (token) => {
